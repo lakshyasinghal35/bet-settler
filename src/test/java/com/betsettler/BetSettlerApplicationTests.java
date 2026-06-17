c2d7b525fa2372a3f7d2610788076da9aa10e6bc
lakshya <lakshyasinghal333@gmail.com>
2026-06-17 16:41:16 +0530

    Initial commit: Spring Boot bet-settler project

---
+++ src/test/java/com/betsettler/BetSettlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsettler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetSettlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
